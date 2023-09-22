1570fcfa3da0ba2f8b6a1a24b2e0fc0dad8e06b3
Jonas Lalin <1ac6df24@gmail.com>
2023-09-22 10:55:49 +0200

    add cross import module

--- mypackage1/otherpkg/subpkg1/sigmoid.py
+++ mypackage1/otherpkg/subpkg1/sigmoid.py
@@ -1,6 +1,11 @@
 from math import exp
 from typing import Union
+from mypkg.subpkg2.goodbye import goodbye
 
 
 def sigmoid(x: Union[float, int]) -> float:
     return 1 / (1 + exp(-x))
+
+
+def goodbye_sigmoid(x: Union[float, int]) -> str:
+    return goodbye(str(sigmoid(x)))
--- mypackage1/tests/test_otherpkg.py
+++ mypackage1/tests/test_otherpkg.py
@@ -1,6 +1,11 @@
 from otherpkg.reduce import reduce
+from otherpkg.subpkg1.sigmoid import goodbye_sigmoid
 
 
-def test_sum() -> None:
+def test_reduce() -> None:
     assert reduce(1, 2, 3, 4, 5, acc=0) == 15
     assert reduce("a", "b", "c", acc="") == "abc"
+
+
+def test_goodbye_sigmoid() -> None:
+    assert goodbye_sigmoid(0) == "Goodbye, 0.5!"
