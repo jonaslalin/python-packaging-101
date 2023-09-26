8681170cbe705c8af12c2085164ff43b11cb3154
Jonas Lalin <1ac6df24@gmail.com>
2023-09-26 16:32:40 +0200

    debug test config

--- mypackage1/mypkg/subpkg1/fibo.py
+++ mypackage1/mypkg/subpkg1/fibo.py
@@ -4,9 +4,12 @@ from fibo import fib2
 
 
 def fib2_inclusive(n: int) -> List[int]:
+    if n == 0:
+        return [0]
+    elif n == 1:
+        return [0, 1, 1]
     result = fib2(n)
-    a, b = result[-2], result[-1]
-    a, b = b, a + b
-    if b == n:
-        result.append(b)
+    a, b = result[-2:]
+    if (c := a + b) == n:
+        result.append(c)
     return result
--- mypackage1/tests/test_mypkg.py
+++ mypackage1/tests/test_mypkg.py
@@ -1,3 +1,7 @@
+from typing import List
+
+import pytest
+
 from mypkg.subpkg1.fibo import fib2_inclusive
 from mypkg.subpkg1.hello import hello
 
@@ -6,6 +10,20 @@ def test_fib2_inclusive() -> None:
     assert fib2_inclusive(7) == [0, 1, 1, 2, 3, 5]
     assert fib2_inclusive(8) == [0, 1, 1, 2, 3, 5, 8]
     assert fib2_inclusive(9) == [0, 1, 1, 2, 3, 5, 8]
+
+
+@pytest.mark.parametrize(
+    ("n", "expected"),
+    [
+        (0, [0]),
+        (1, [0, 1, 1]),
+        (2, [0, 1, 1, 2]),
+        (3, [0, 1, 1, 2, 3]),
+        (4, [0, 1, 1, 2, 3]),
+    ],
+)
+def test2_fib2_inclusive(n: int, expected: List[int]) -> None:
+    assert fib2_inclusive(n) == expected
 
 
 def test_hello() -> None:
