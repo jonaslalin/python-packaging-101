bc41cda249407c5182cb14fe9d084228976cef4a
Jonas Lalin <1ac6df24@gmail.com>
2023-09-27 11:16:38 +0200

    type error example

---
+++ mypackage1/otherpkg/subpkg1/subsubpkg1/mypy.py
@@ -0,0 +1,14 @@
+def greeting(name: str) -> str:
+    return "Hello " + name
+
+
+def contains_type_error() -> str:
+    import sys
+
+    if sys.version_info >= (3, 10):
+        return greeting(123)
+    if sys.version_info >= (3, 9):
+        return greeting(b"Alice")
+    if sys.version_info >= (3, 8):
+        return greeting("Bob")
+    return greeting(True)
